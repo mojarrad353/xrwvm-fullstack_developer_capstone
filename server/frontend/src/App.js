acf7020324d5d4035939303feeeb9a343c89ab4b
mojarrad353 <m.mojarrad353@gmail.com>
2026-01-30 12:57:44 -0500

    update

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,11 +1,14 @@
-import LoginPanel from "./components/Login/Login"
+import LoginPanel from "./components/Login/Login";
+
 import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<LoginPanel />} />
+      
     </Routes>
   );
 }
+
 export default App;
